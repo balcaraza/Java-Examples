c511433b5c8980d67beaf0b705acf5d7277791c2
Balam <balcaraza@gmail.com>
2025-01-12 16:03:48 -0600

    ejercicio suma

---
+++ Cap2/src/Bienvenido4.java
@@ -0,0 +1,9 @@
+
+public class Bienvenido4 {
+
+	public static void main(String[] args) {
+		System.out.printf("%s%n%s%n", "Bienvenido a", "La programacion en java!");
+
+	}
+
+}
---
+++ Cap2/src/Suma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int numero1, numero2, suma;
+		
+		System.out.println("Escriba el primer numero entero");
+		numero1= entrada.nextInt();
+		System.out.println("Escriba el segundo numero entero");
+		numero2= entrada.nextInt();
+		
+		suma = numero1 + numero2;
+		
+		System.out.printf("La suma es %d%n", suma);
+
+	}
+
+}
