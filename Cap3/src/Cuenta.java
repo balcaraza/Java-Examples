74fc10dc3ecf6174733b726148100ecff2db9225
Balam <balcaraza@gmail.com>
2025-02-17 23:29:34 -0600

    metodos de instancia

--- Cap3/src/Cuenta.java
+++ Cap3/src/Cuenta.java
@@ -1,23 +1,39 @@
-//Fig 3.5 Cuenta.java
+//Fig 3.8 Cuenta.java
 //Clase Cuenta con un constructor que inicializa el nombre
 
 public class Cuenta {
 	
 		private String nombre;
+		private double saldo;
 
-		//el contructor inicializa con nombre en el parametro nombre
-	public Cuenta(String nombre){
+
+		//Constructor con dos parametros
+	public Cuenta(String nombre, double saldo){
+
+		this.nombre = nombre;
+
+		//valida que saldo sea mayor a 0.0, de lo contrario
+		//la variable saldo mantiene su valor incial predeterminado de 0.0
+		if (saldo >0.0)
+			this.saldo = saldo;
+	}
+	//metodo que deposita (suma) solo una cantidad valida al saldo
+	public void depositar(double montoDeposito){
+		if (montoDeposito > 0.0)
+			saldo = saldo +montoDeposito;
+	}
+		//metodo que devuelve el saldo de la cuenta
+	public double obtenerSaldo(){
+		return saldo;
+	}
+
+	//metodo que establece el nombre
+	public void estableceNombre(String nombre){
 		this.nombre = nombre;
 	}
-		
-		//metodo para establecer nombre en el objeto
-		public void establecerNombre(String nombre) {
-			this.nombre = nombre; //almacena el nombre
-		}
-		
-		public String obtenerNombre() {
-			return nombre; //devuelve el valor del nombre a quien lo invoco.
-			
-		}	
+	 //metodo que devuelve el nombre
+	public String obtenerNombre(){
+		return nombre;
+	}
 
 }
--- Cap3/src/PruebaCuenta.java
+++ Cap3/src/PruebaCuenta.java
@@ -6,12 +6,33 @@ import java.util.Scanner;
 public class PruebaCuenta {
 
 	public static void main(String[] args) {
-		Cuenta cuenta1 = new Cuenta("Jane Green");
-		Cuenta cuenta2 = new Cuenta("John Blue");
+		Cuenta cuenta1 = new Cuenta("Jane Green", 50.00);
+		Cuenta cuenta2 = new Cuenta("John Blue", -7.53);
 
-		//Muestra el valor incial de nombre para cada Cuenta
-		System.out.printf("El nombre de cuenta1 es: %s%n", cuenta1.obtenerNombre());
-		System.out.printf("El nombre de cuenta2 es: %s%n", cuenta2.obtenerNombre());
+		//Muestra el valor saldo inicial de cada objeto
+		System.out.printf("Saldo de %s: $%.2f%n", cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+		System.out.printf("Saldo de %s: $%.2f%n", cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Escriba el moto a depositar para la cuenta1: ");
+		double montoDeposito = entrada.nextDouble();
+		System.out.printf("%nsumando %.2f al saldo d cuenta1%n%n", montoDeposito);
+		cuenta1.depositar(montoDeposito);//suma al saldo de cuenta1
+
+		//muestra saldos
+		System.out.printf("Saldo de %s: $%.2f%n", cuenta1.obtenerNombre(),cuenta1.obtenerSaldo());
+		System.out.printf("Saldo de %s: $%.2f%n", cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+
+		System.out.print("Escruba el monto a depositar para la cuenta 2: ");
+		montoDeposito = entrada.nextDouble();;
+		System.out.printf("%nsumando %.2f al saldo de cuenta2%n%n", montoDeposito);
+		cuenta2.depositar(montoDeposito);
+
+		//muestra los saldos
+		System.out.printf("Saldo de %s: $%.2f%n", cuenta1.obtenerNombre(),cuenta1.obtenerSaldo());
+		System.out.printf("Saldo de %s: $%.2f%n%n", cuenta2.obtenerNombre(),cuenta2.obtenerSaldo());
+
 
 	}
 
