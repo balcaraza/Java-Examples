2228c40d2dd39c11e7b03b155fe7d116d1ec62b1
Balam <balcaraza@gmail.com>
2025-08-11 17:39:08 -0600

    Problema del else suelto 1

---
+++ Cap4/src/main/ElseSuelto.java
@@ -0,0 +1,31 @@
+package main;
+
+public class ElseSuelto {
+	public static void main(String[] args) {
+		
+		int x=11;
+		int y=9;
+		
+		if(x<10)
+		if(y>10)
+		System.out.println("*****");
+		else
+		System.out.println("#####");
+		System.out.println("$$$$$");
+		
+		System.out.println();
+		System.out.println();
+		
+		if(x<10)
+		{
+		System.out.println("*****");
+		}
+		else
+		{
+		System.out.println("#####");
+		System.out.println("$$$$$");
+		}
+		
+	}
+
+}
