ccfa1c549e3fb8a03fa541800927ec168a3eb535
Balam <balcaraza@gmail.com>
2025-08-11 17:49:51 -0600

    Problema Else suelto 2

---
+++ Cap4/src/main/ElseSuelto2.java
@@ -0,0 +1,20 @@
+package main;
+
+public class ElseSuelto2 {
+	public static void main(String[] args) {
+		int x=5;
+		int y=8;
+	
+		if(y==8) {
+			if(x==5) {
+				System.out.println("@@@@@");
+			}
+		} else {
+		System.out.println("#####");
+		}
+		System.out.println("$$$$$");
+		System.out.println("&&&&&");
+		
+	}
+
+}
