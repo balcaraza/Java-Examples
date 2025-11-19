d806afa124c3bea3543d6389b1f434e26f3d4c9b
Balam Alcaraz <balcaraza@gmail.com>
2025-11-19 16:48:46 -0600

    Refactoring de proyecto

--- Cap2/src/Bienvenido2.java
+++
@@ -1,9 +0,0 @@
-//Fig 2.3 Bienvenido2.java
-//Imprimir una linea de texto con varias instruccíones
-public class Bienvenido2 
-{
-	public static void main(String[] args) {
-		System.out.print("Bienvenido a ");
-		System.out.println("la programacion en Java!");
-	}
-}
--- Cap2/src/Bienvenido3.java
+++
@@ -1,7 +0,0 @@
-//Imprimir varias lineas de texto con una instruccion
-
-public class Bienvenido3 {
-	public static void main(String [] args) {
-		System.out.println("Bienvenido\na\nla programacion \nen Java!");
-	}
-}
