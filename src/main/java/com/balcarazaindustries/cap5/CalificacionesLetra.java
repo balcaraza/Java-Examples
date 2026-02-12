e6e9624cb6e2763cbc51530736767282202bc225
Balam Alcaraz <balcaraza@gmail.com>
2026-02-12 11:40:45 -0600

    Uso de switch case

---
+++ src/main/java/com/balcarazaindustries/cap5/CalificacionesLetra.java
@@ -0,0 +1,69 @@
+package com.balcarazaindustries.cap5;
+
+import java.util.Scanner;
+
+public class CalificacionesLetra {
+    public static void    main(String[] args) {
+        int total =0;
+        int contadorCalif =0;
+        int aCuenta =0;
+        int bCuenta =0;
+        int cCuenta =0;
+        int dCuenta =0;
+        int fCuenta =0;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.printf("%s%n%s%n     %s%n    %s%n", "introduzca las calificaciones en el rango de 0-100",
+                "Escriba el indicador de fin de archivo para terminar la entrada:",
+                "En linux/macOS escriba <Ctrl> d + intro",
+                "En windows escriba <Ctrl> z + intro");
+
+        while(entrada.hasNext()){
+            int calificacion = entrada.nextInt();
+            total+= calificacion;
+            ++contadorCalif;
+
+            switch (calificacion/10){
+                case 9:
+                case 10:
+                    ++aCuenta;
+                    break;
+
+                case 8:
+                    ++bCuenta;
+                    break;
+
+                case 7:
+                    ++cCuenta;
+                    break;
+
+                case 6:
+                    ++dCuenta;
+                    break;
+
+                default:
+                    ++fCuenta;
+                    break;
+            }
+        }
+        System.out.printf("%nReporte de calificaciones:%n");
+
+        if (contadorCalif!=0){
+            double promedio = (double) total / contadorCalif;
+
+            System.out.printf("El total de las %d calificaciones introducidas es %d%n", contadorCalif, total);
+            System.out.printf("El promedio de la clase es %.2f%n", promedio);
+
+            System.out.printf("%n%s%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n",
+                    "Numero de estudiantes que recibieron cada calificacion:",
+                    "A: ", aCuenta,
+                    "B: ", bCuenta,
+                    "C: ", cCuenta,
+                    "D: ", dCuenta,
+                    "F: ", fCuenta);
+        } else {
+            System.out.println("no se introdujeron calificaciones");
+        }
+    }
+}
